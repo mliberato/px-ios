8ff25edcfa5fcdbf4f5d7fddf71ef77040945135
augustocollerone <collerone.augusto@gmail.com>
2018-02-22 14:25:32 -0300

    Poc Localizator

---
+++ MercadoPagoSDK/MercadoPagoSDK/poc_Localizator.swift
@@ -0,0 +1,52 @@
+//
+//  poc_Localizator.swift
+//  MercadoPagoSDK
+//
+//  Created by AUGUSTO COLLERONE ALFONSO on 22/2/18.
+//  Copyright © 2018 MercadoPago. All rights reserved.
+//
+
+import Foundation
+
+private class poc_Localizator {
+    
+    static let sharedInstance = poc_Localizator()
+    
+    lazy var localizableDictionary: NSDictionary! = {
+        let bundle = MercadoPago.getBundle()
+        
+        if let path = bundle?.path(forResource: "poc_translations", ofType: "plist") {
+            return NSDictionary(contentsOfFile: path)
+        }
+        fatalError("Localizable file NOT found")
+    }()
+    
+    func localize(string: String) -> String {
+        let languageID = MercadoPagoContext.getLanguage()
+        let parentlanguageID = MercadoPagoContext.getParentLanguage()
+        
+        guard let localizedStringDictionary = localizableDictionary.value(forKey: string) as? NSDictionary, let localizedString = localizedStringDictionary.value(forKey: languageID) as? String else {
+            
+            
+            let localizedStringDictionary = localizableDictionary.value(forKey: string) as? NSDictionary
+            
+            if let parentLocalizedString = localizedStringDictionary?.value(forKey: parentlanguageID) as? String {
+                return parentLocalizedString
+            }
+            
+            #if DEBUG
+                assertionFailure("Missing translation for: \(string)")
+            #endif
+            
+            return ""
+        }
+        
+        return localizedString
+    }
+}
+
+extension String {
+    var localized_poc: String {
+        return poc_Localizator.sharedInstance.localize(string: self)
+    }
+}
